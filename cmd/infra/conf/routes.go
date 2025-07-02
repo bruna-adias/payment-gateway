cabb5ba6d5d9aaa69107f919aec61d9a80f26dfd
Leonardo Borges <beolorges@gmail.com>
2025-07-01 22:03:26 -0300

    feat: create health route

--- cmd/infra/conf/routes.go
+++ cmd/infra/conf/routes.go
@@ -8,4 +8,11 @@ func Routes(engine *gin.Engine, run *Runtime) {
 	engine.POST("/payments", run.CreatePaymentHandler.Execute)
 	engine.POST("/payments/:id/process", run.ProcessPaymentHandler.Execute)
 	engine.GET("/orders/:id", run.GetCashoutHandler.Execute)
+	engine.GET("/health", HealthHandler())
+}
+
+func HealthHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"status": "ok"})
+	}
 }
