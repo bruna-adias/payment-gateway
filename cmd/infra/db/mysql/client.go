f484d8c602c550623a35a58397c825c9d4e22185
Leonardo Borges <beolorges@gmail.com>
2025-07-01 21:54:02 -0300

    fix: reconnection to database when starting up

--- cmd/infra/db/mysql/client.go
+++ cmd/infra/db/mysql/client.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"payment-gateway/cmd/infra"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,13 +13,27 @@ func NewMySQLClient(cfg *infra.Configuration) (*sql.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbName)
 
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open database: %w", err)
+	var db *sql.DB
+	var err error
+	for i := 0; i < 15; i++ {
+		db, err = sql.Open("mysql", dsn)
+
+		if err == nil {
+			if err := db.Ping(); err == nil {
+				break
+			}
+
+			db.Close()
+		}
+
+		fmt.Printf("failed to open database: %v\n", err)
+
+		fmt.Printf("waiting for 5 seconds before retrying...\n")
+		time.Sleep(5 * time.Second)
 	}
 
-	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("failed to ping database: %w", err)
+	if err != nil {
+		return nil, err
 	}
 
 	return db, nil
